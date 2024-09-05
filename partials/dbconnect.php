08654d08010939795bf91793b5d50a59fa06f482
Mohit Patil <113899190+snake0507@users.noreply.github.com>
2024-09-06 00:55:33 +0530

    Add files via upload

---
+++ partials/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+$username = "root";
+$password = "";
+$server = "localhost";
+$database = "users101";
+
+$conn = mysqli_connect($server,$username,$password,$database);
+if(!$conn){
+    die("Error" . mysqli_connect_error());
+} 
+?>
